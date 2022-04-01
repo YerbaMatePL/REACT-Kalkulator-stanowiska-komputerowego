25e7de2747079f38b46d8543b3d515b40c55904d
Ewelina <ewela.m95@gmail.com>
2022-04-01 14:51:43 +0200

    added Select component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,15 @@
 import './Form.css';
+import Select from './Select/Select';
+
 
 function Form() {
+
 	return (
 		<div className='form'>
 			<div className='form__items'>
 				<div className='form__item'>
 					<div className='form__item_labels'>
-						<label for='item'>przedmiot</label>
+						<label htmlFor='item'>przedmiot</label>
 					</div>
 					<div className='form__item_inputs'>
 						<input type='text' id='item' placeholder='Podaj nazwę przedmiotu' />
@@ -15,52 +18,36 @@ function Form() {
 
 				<div className='form__item'>
 					<div className='form__item_labels'>
-						<label for='description'>Opis</label>
+						<label htmlFor='description'>Opis</label>
 					</div>
 					<div className='form__item_inputs'>
 						<input
 							type='text'
 							id='description'
-							placeholder='Wpisz krótki opis np. model, marka przedmiotu'
+							placeholder='Wpisz krótki opis np. model'
+						/>
+					</div>
+				</div>
+				<Select />
+				<div className='form__item'>
+					<div className='form__item_labels'>
+						<label htmlFor='price'>Cena</label>
+					</div>
+					<div className='form__item_inputs'>
+						<input
+							type='number'
+							step='0.5'
+							id='price'
+							placeholder='Wpisz cenę przedmiotu'
 						/>
 					</div>
 				</div>
 
 				<div className='form__item'>
-					<div className='form__item_labels'>
-						<label for='categories'>Kategoria</label>
-					</div>
-					<div className='form__item_inputs'>
-						<select id='categories'>
-							<option selected value='Sprzęt'>
-								Sprzęt
-							</option>
-							<option value='Oprogramowanie'>Oprogramowanie</option>
-							<option value='Myszki'>Myszki</option>
-						</select>
+					<div className='form__item__btnAdd'>
+						<button>Dodaj</button>
 					</div>
 				</div>
-
-				<div className='form__item'>
-					<div className='form__item_labels'>
-						<label for='price'>Cena</label>
-					</div>
-                    <div className='form__item_inputs'>
-					<input
-						type='number'
-						step='0.5'
-						id='price'
-						placeholder='Wpisz cenę przedmiotu'
-					/>
-                    </div>
-				</div>
-
-                <div className='form__item'>
-                    <div className="form__item__btnAdd">
-                    <button>Dodaj</button>
-                    </div>
-                </div>
-
 			</div>
 		</div>
 	);
